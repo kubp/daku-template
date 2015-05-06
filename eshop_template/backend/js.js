b7c5b9345d5f46dd47305e4f64f0bc644ecd6d37
Hribek <Ondrek7@seznam.cz>
2015-05-06 15:12:58 +0200

    Color, graph, delete-confirm,

--- eshop_template/backend/js.js
+++ eshop_template/backend/js.js
@@ -1,16 +1,33 @@
+$(document).ready(function() {
+$('#table-edit').dataTable( {
+  "ajax": {
+    "url": "example.php",
+    "type": "POST"
+  },
+  "data": [
+      
+        ]
+   } );
+} );
+
+
 function del(r){            //smazání řádku
+  var question=confirm("Přejete si opravdu smazat?");
+  if(question){
 		var delete_row = r.parentNode.parentNode.rowIndex;
    		document.getElementById("table-edit").deleteRow(delete_row);
+    }
 }
 function create_open(){ 
-		document.getElementById('create-row').style.display ='block';
+    document.getElementById('create-row').style.height ='100px';
 }
 
 function cancel(){
-		document.getElementById('create-row').style.display ='none';
+		document.getElementById('create-row').style.height ='0px';
 }	
 
-function create(){            //Vytvoření nového řádku
+function create(){ 
+            //Vytvoření nového řádku
 		var edit_table = document.getElementById("table-edit");
 		var radek = edit_table.insertRow(1);
 		var cell = radek.insertCell(0);
@@ -33,15 +50,22 @@ function create(){            //Vytvoření nového řádku
 		var cell = radek.insertCell(0);
 		var get = document.getElementById("ProductNo").value;
 		cell.innerHTML = get;
+   document.getElementById("ProductNo").value="";
 		var cell = radek.insertCell(0);
 		var get = document.getElementById("category").value;
 		cell.innerHTML = get;
+       document.getElementById("category").value="";
 		var cell = radek.insertCell(0);
 		var get = document.getElementById("kosik").value;
 		cell.innerHTML = get;
+       document.getElementById("kosik").value="";
 		var cell = radek.insertCell(0);
 		var get = document.getElementById("name").value;
-		cell.innerHTML = get;
+		cell.innerHTML = "<a href=index.html>"+get+"</a>";
+    cell.setAttribute("href", "a.html")
+       document.getElementById("name").value="";
+ 
+
 		var cell = radek.insertCell(0);
 		var get = document.getElementById("id").value;
 		cell.innerHTML = get;
@@ -50,6 +74,8 @@ function create(){            //Vytvoření nového řádku
 
 
 function edit(r){   //vytvoření řádku na úpravu
+  
+
 
 	var i = r.parentNode.parentNode.rowIndex;
 	var edit_table = document.getElementById("table-edit");
@@ -100,6 +126,7 @@ function edit(r){   //vytvoření řádku na úpravu
     cell.appendChild(submit);
 	submit.setAttribute("id", "First_cell");
 
+
 	var cell = radek.insertCell(0);
 }
 
@@ -113,7 +140,9 @@ function edit_row(r){            //Úprava konkrétního řádku
 	var i = r.parentNode.parentNode.rowIndex;
    	var x = document.getElementById("table-edit").rows[i-1].cells;
  		if(a!=0){
-   			x[1].innerHTML = a;
+   			x[1].innerHTML = "<a href=index.html>"+a+"</a>";
+      
+     
 		}
  		if(b!=0){
    			x[2].innerHTML = b;
@@ -124,6 +153,48 @@ function edit_row(r){            //Úprava konkrétního řádku
   		 if(d!=0){
    			x[4].innerHTML = d;
    		}
+  
+      
     var delete_row = r.parentNode.parentNode.rowIndex;
     document.getElementById("table-edit").deleteRow(delete_row);
+  
+
+
 }
+
+
+
+google.load('visualization', '1', {packages: ['corechart', 'line']});
+google.setOnLoadCallback(day);
+
+//Počet prodaných za  hodinu
+function day() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'počet');
+
+
+      data.addRows([
+        [0, 0],   [1, 10],  [2, 12],  [3, 14],  [4, 17],  [5, 19],
+        [6, 22],  [7, 23],  [8, 28],  [9, 31],  [10, 32], [11, 35],
+        [12, 37],[13,40], [14, 43],   [15, 70],  [16, 80],  [17, 83], 
+         [18, 85],  [19, 90],
+        [20, 11],  [21, 27],  [22, 33],  [23, 40],  [24, 32]
+      ]);
+
+      var options = {
+        hAxis: {
+
+          title: 'Hodiny',
+           ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+        },
+        vAxis: {
+          title: 'Prodané zboží'
+        },
+        backgroundColor: 'RGB(242,252,255)',
+
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart'));
+      chart.draw(data, options);
+    }
